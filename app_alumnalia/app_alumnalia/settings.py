92114b052af4a34aaf7d40bc8b12820b8ed76e6b
Gisela Palomar <gslpalomar02@gmail.com>
2024-12-13 15:36:07 +0100

    feat: no hay debug tool

--- app_alumnalia/app_alumnalia/settings.py
+++ app_alumnalia/app_alumnalia/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app_alumnalia",
-    'debug_toolbar'
+#    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+ #   'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = "app_alumnalia.urls"
--- app_alumnalia/app_alumnalia/urls.py
+++ app_alumnalia/app_alumnalia/urls.py
@@ -9,7 +9,7 @@ app_name= "app_alumnalia"
 urlpatterns = [
     path("", views.InicioView.as_view(), name= 'inicio'),
     path("inicio/", views.InicioView.as_view(), name= 'inicio'),
-    path('__debug__/', include('debug_toolbar.urls')),
+#    path('__debug__/', include('debug_toolbar.urls')),
     path("datos_personales/", views.datos_personales_view.as_view(), name= 'datos_personales'),
     path('datos_formador/', views.datos_formador_view.as_view(), name='datos_formador')
 ]
